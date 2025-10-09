27393fc6755442a9a4af6f72b35ea0926628295b
JalemOG <janikhamilton1441@gmail.com>
2025-10-09 15:50:46 -0600

    Se arreglan un pichazo de errores de nuevoV2

--- obj/Debug/net8.0-windows/CrazyRisk.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/CrazyRisk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrazyRisk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6469f657b6f07490cbdf9dc74f69d77d8a3050d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a9fedfc1aed7c38d5cc13e4a0606dd443f379f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrazyRisk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrazyRisk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
